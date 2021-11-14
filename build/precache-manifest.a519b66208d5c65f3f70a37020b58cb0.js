542a35ce4a0cdf7fd8c8565c2a720cd18b68f0d3
liyang <18382924081@163.com>
2021-11-14 15:49:05 +0800

    'build'

---
+++ build/precache-manifest.a519b66208d5c65f3f70a37020b58cb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2340a7b3e79762877e7c3c3c65f3a05c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6dc0fa918948c50358c",
+    "url": "/static/js/2.f91b9302.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f91b9302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bbe0c537ed0f5b72b3",
+    "url": "/static/js/main.aa39b358.chunk.js"
+  },
+  {
+    "revision": "306484cd6b59a38fd812",
+    "url": "/static/js/runtime-main.88b78cf9.js"
+  },
+  {
+    "revision": "2e7d7a757c4d42e3eddc9b366b772329",
+    "url": "/static/media/chakra-ui-dark.2e7d7a75.png"
+  },
+  {
+    "revision": "39278f4efbc1bb779fe8af246dbac52d",
+    "url": "/static/media/chakra-ui-light.39278f4e.png"
+  },
+  {
+    "revision": "276e3cd8810c461e484a77e4d02ffde5",
+    "url": "/static/media/chakra-ui.276e3cd8.png"
+  }
+]);
